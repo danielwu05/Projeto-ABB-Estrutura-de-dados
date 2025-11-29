1b1e55070ebdcf19f0120a76935f9c1895406e4d
Daniel Wu <danielwu288@gmail.com>
2025-11-29 10:52:03 -0300

    terminado

--- PROJETO2.c
+++ PROJETO2.c
@@ -6,233 +6,6 @@
 #include <ctype.h>
 
 
-NoArvore* inserirNovaVenda(Arvore* arvore)
-{
-    NoArvore *nova = (NoArvore*)malloc(sizeof(NoArvore));
-    Data data;
-    char cliente[51];
-    char vendedor[51];
-    char matricula[5];
-    float valor;
-    int id;
-
-    printf("\n=== Cadastro de Nova Venda ===\n");
-
-    int min = 1000;
-    int max = 9999;
-
-    id = (rand() % (max - min + 1)) + min;
-
-    printf("ID gerado: %d\n", id);
-
-     while (getchar() != '\n');
-
-    printf("Digite o nome do Cliente: ");
-    gets(cliente);
-
-    printf("Digite o nome do Vendedor: ");
-    gets(vendedor);
-
-
-    int aconteceu = 0;
-    percorreArvore(arvore->raiz,vendedor,matricula,&aconteceu);
-
-    if(aconteceu==0)
-        {
-            gerarMatricula(matricula);
-            printf("Matricula gerada e:%s\n",matricula);
-        }
-    else
-    {
-            printf("Vendedor ja registrado, matricula : %s\n", matricula);
-    }
-
-
-
-    printf("Digite a data (dia mes ano): ");
-    scanf("%d %d %d", &data.dia, &data.mes, &data.ano);
-
-    printf("Digite o valor da venda: ");
-    scanf("%f", &valor);
-
-    printf("\nVenda confirmada!\n");
-
-    arvore->raiz = InsereArvore(arvore->raiz, id, cliente, vendedor, matricula, data, valor);
-
-    return arvore->raiz;
-}
-
-
-
-void buscarVendasVendedor(NoArvore *no, char *busca, int porMatricula)
-{
-    if(no!=NULL)
-    {
-            if ((porMatricula && strcmp(no->info.matricula, busca) == 0) || (!porMatricula && strcmp(no->info.vendedor, busca) == 0))
-                {
-                    imprimeNo(no);
-                }
-    buscarVendasVendedor(no->right, busca, porMatricula);
-    buscarVendasVendedor(no->left, busca, porMatricula);
-    }
-    else
-    {
-        printf("Nao ha venda registrada");
-    }
-}
-
-
-void buscarPorMatricula(NoArvore *no, char *matricula)
-{
-     if(no != NULL)
-{
-        if (strcmp(no->info.matricula, matricula) == 0)
-    {
-        imprimeNo(no);
-    }
-
-    buscarPorMatricula(no->right, matricula);
-    buscarPorMatricula(no->left, matricula);
-}
- }
-
-
-void buscarPorNome(NoArvore *no, char *nome)
-{
-    if(no != NULL)
-{
-    if (strcmp(no->info.vendedor, nome) == 0)
-    {
-        imprimeNo(no);
-    }
-    buscarPorNome(no->right, nome);
-    buscarPorNome(no->left, nome);
-}
-}
-
-void menuBuscarVendedor(Arvore *a)
-{
-    int opcao;
-    char busca[51];
-
-    if ((VaziaArvore(a))==0)
-    {
-        printf("\nNenhuma venda cadastrada!\n");
-    }
-
-else{
-
-    printf("\n=== Buscar Vendas por Vendedor ===\n");
-    printf("1. Por Matricula\n");
-    printf("2. Por Nome\n");
-    printf("Escolha: ");
-    scanf("%d", &opcao);
-
-    while (opcao != '\n' || opcao != EOF){
-        getchar();
-    }
-
-    if (opcao == 1)
-    {
-        printf("Digite a matricula (ex: V000): ");
-        gets(busca);
-
-        printf("\n--- Vendas Encontradas ---\n");
-        printf("ID | Vendedor | Matricula | Cliente | Data de Transacao | Valor(R$)\n");
-        printf("-------------------------------------------------------------------\n");
-        buscarPorMatricula(a->raiz, busca);
-    }
-    else if (opcao == 2)
-    {
-        printf("Digite o nome do vendedor: ");
-        gets(busca);
-
-        printf("\n Vendas Encontradas \n");
-        printf("ID | Vendedor | Matricula | Cliente | Data de Transacao | Valor(R$)\n");
-        printf("-------------------------------------------------------------------\n");
-        buscarPorNome(a->raiz, busca);
-    }
-    else {
-        printf("\nOpcao de busca invalida.\n");
-    }
-}
-}
-
-void Venda_maior_menor(Arvore *arvore)
-{
-    if(arvore->raiz == NULL)
-    {
-        printf("Nao ha nenhuma venda registrada no momento");
-    }
-    else
-    {
-        float valor;
-        int opcao;
-
-        printf("Digite 1 para procurar vendas maiores ou 2 para vendas menores ");
-        scanf("%d",&opcao);
-        printf("Digite o valor de referencia:");
-        scanf("%f",&valor);
-
-        imprimeAcimaouAbaixo(arvore->raiz,valor,opcao);
-    }
-}
-
-void estatisticas(Arvore *arvore)
-{
-    if (VaziaArvore(arvore)== 1)
-{
-    int totalVendas = quantidadeNos(arvore->raiz, 0);
-    float faturamentoTotal = somaValores(arvore->raiz);
-
-    printf("\n=== Estatisticas ===\n");
-    printf("Total de vendas: %d\n", totalVendas);
-    printf("Faturamento total: R$ %.2f", faturamentoTotal); //Apenas fiz isso para garantir que o numero seja no formato pedido de 2 casas decimais
-}
-
-    else
-    {
-        printf("Nao ha venda registrada no momento");
-    }
-
-}
-
-
-Arvore* removerVenda(Arvore*arvore)
-{
-    int id;
-
-    if ((VaziaArvore(arvore))==1)
-    {
-    printf("\n Remover Venda\n");
-    printf("Digite o ID da venda a ser removida: ");
-    scanf("%d", &id);
-
-
-    if (BuscarArvore(arvore->raiz, id))
-    {
-        printf("\nVenda removida!\n");
-        arvore->raiz = RemoveGalho(arvore->raiz, id);
-        return arvore;
-
-    }
-    else
-    {
-        printf("Venda com ID %d nao encontrada!\n", id);
-        return arvore;
-    }
-    }
-
-else
-{
-        printf("\nNenhuma venda cadastrada para remover.\n");
-        return arvore;
-}
-}
-
-
-
-
 
 int main() {
     srand(time(NULL));
@@ -250,9 +23,8 @@ int main() {
         printf("7. Finalizar sistema\n");
         printf("Escolha: ");
         scanf("%d", &opcao);
-
-        switch (opcao)
-        {
+        empty_stdin();
+        switch (opcao){
             case 1:
                  inserirNovaVenda(arvore);
                 break;
@@ -273,21 +45,16 @@ int main() {
                 break;
             case 7:
                 RemoveArvore(arvore);
+                return 0;
                 break;
             default:
                 printf("\nOpcao invalida!\n");
+                system("pause");
+                break;
         }
-
-    if (opcao != 7)
-    {
-    printf("\nPressione Enter para continuar...");
-    while(getchar() != '\n');
-    getchar();
+    if (opcao != 7){
+        printf("\nPressione Enter para continuar...");
     }
-    }
-
-    while(opcao!=7);
-
+    }while(opcao<8);
     return 0;
-
 }
--- arvore.h
+++ arvore.h
@@ -9,7 +9,6 @@
 #include <string.h>
 #include <ctype.h>
 
-
 typedef struct data {
     int dia;
     int mes;
@@ -45,25 +44,37 @@ Arvore* IniciaArvore() {
     return novaArvore;
 }
 
-void percorreArvore(NoArvore* arvore, char *a, char*matricula, int *aconteceu){
+void empty_stdin (void)
+{
+    int c = getchar();
+
+    while (c != '\n' && c != EOF)
+        c = getchar();
+}
+
+
+
+
+char * percorreArvore(NoArvore* arvore, char *a){
     if (arvore != NULL){
-
         if(strcasecmp(arvore->info.vendedor, a)==0){
-            strcpy(matricula, arvore->info.matricula);
-            *aconteceu = 1;
+            return arvore->info.matricula;
         }
-        else{
-            *aconteceu = 0;
+        char* matricula = percorreArvore(arvore->left, a);
+        if (matricula != NULL) {
+            return matricula;
         }
-        percorreArvore(arvore->left,a,matricula,aconteceu);
-        percorreArvore(arvore->right,a,matricula,aconteceu);
+        matricula = percorreArvore(arvore->right, a);
+        if (matricula != NULL) {
+            return matricula;
+        }
+        return NULL;
     }
-
+    return NULL;
 }
 
 int VaziaArvore(Arvore* arvore) {
-    if (arvore->raiz == NULL)
-    {
+    if (arvore->raiz == NULL){
         return 0;
     }
     return 1;
@@ -144,25 +155,20 @@ NoArvore* RemoveGalho(NoArvore* no, int achar) {
     return no;
 }
 
-void LiberaNos(NoArvore* no)
-{
-        if(no->right != NULL)
-        {
+void LiberaNos(NoArvore* no){
+    if(no->right != NULL){
             LiberaNos(no->right);
         }
-        if(no->left != NULL)
-    {
+        if(no->left != NULL){
         LiberaNos(no->left);
     }
         free(no);
 }
 
 Arvore* RemoveArvore(Arvore* arvore) {
-    if (arvore -> raiz == NULL)
-    {
+    if (arvore -> raiz == NULL){
         return NULL;
     }
-
     LiberaNos(arvore->raiz);
     free(arvore);
     return NULL;
@@ -219,9 +225,30 @@ if (root == NULL)
     return root;
 }
 
+void desenharLinha() {
+    printf("+--------+-----------------+-----------+-----------------+------------+-------------+\n");
+}
+
+void desenharDivisor() {
+    printf("+--------+-----------------+------------+-------------+\n");
+}
+
+void lerTexto(char* buffer, int tamanho) {
+    fgets(buffer, tamanho, stdin);
+    size_t len = strlen(buffer);
+    if (len > 0 && buffer[len - 1] == '\n') {
+        buffer[len - 1] = '\0';
+    }
+}
 
 void imprimeNo(NoArvore* no) {
-    printf("%d | %s | %s | %s | %02d/%02d/%04d | R$ %.2f\n",no->info.id, no->info.vendedor, no->info.matricula,no->info.cliente, no->info.data.dia, no->info.data.mes,no->info.data.ano, no->info.valor);
+    printf("| %-6d | %-15.15s | %-9s | %-15.15s | %02d/%02d/%04d | R$ %9.2f |\n",no->info.id,no->info.vendedor,no->info.matricula,no->info.cliente,no->info.data.dia, no->info.data.mes, no->info.data.ano,no->info.valor);
+}
+
+void imprimeNoBusca(NoArvore* no) {
+    if (no != NULL){
+        printf("| %06d | %-15.15s | %02d/%02d/%04d | R$ %9.2f |\n",no->info.id,no->info.cliente,no->info.data.dia, no->info.data.mes, no->info.data.ano,no->info.valor);
+    }
 }
 
 void imprimeVendas(NoArvore* no) {
@@ -232,22 +259,16 @@ void imprimeVendas(NoArvore* no) {
 
 void imprimeAcimaouAbaixo(NoArvore* no, float valor, int o)
 {
-    if(no!= NULL)
-{
-
+    if(no!= NULL){
     imprimeAcimaouAbaixo(no->left, valor, o);
-
-    if (o == 1 && no->info.valor > valor)
-    {
+    if (o == 1 && no->info.valor > valor){
         imprimeNo(no);
     }
-    else if (o == 2 && no->info.valor < valor)
-    {
+    else if (o == 2 && no->info.valor < valor){
         imprimeNo(no);
     }
-
     imprimeAcimaouAbaixo(no->right, valor, o);
-}
+    }
 }
 
 void imprimearvoreOrder(NoArvore* no, int o)
@@ -261,7 +282,7 @@ void imprimearvoreOrder(NoArvore* no, int o)
         imprimeNo(no);
         imprimearvoreOrder(no->right, o);
     } else
-    { // Ordem decrescente: direita, raiz, esquerda
+    {
         imprimearvoreOrder(no->right, o);
         imprimeNo(no);
         imprimearvoreOrder(no->left, o);
@@ -282,27 +303,32 @@ else
 {
 
     int opt;
-    printf("\nCrescente [1] ou Decrescente [2]?: ");
-    scanf("%d", &opt);
-
+        printf("\n=== LISTAGEM GERAL DE VENDAS ===\n");
+        printf("Escolha a ordem:\n");
+        printf("[1] Crescente (ID)\n");
+        printf("[2] Decrescente (ID)\n");
+        printf("Opcao: ");
+        scanf("%d", &opt);
+        empty_stdin();
     if (opt == 1 || opt == 2)
-         {
-            printf("\nID | Vendedor | Matrícula | Cliente | Data de Transacao | Valor(R$)\n");
-            printf("-------------------------------------------------------------------\n");
-            imprimearvoreOrder(arvore->raiz, opt);
-        }
+    {
+        printf("\n=== LISTAGEM DE VENDAS ===\n");
+        desenharLinha();
+        printf("| %-6s | %-15s | %-9s | %-15s | %-10s | %-11s |\n",
+               "ID", "Vendedor", "Matricula", "Cliente", "Data", "Valor");
+        desenharLinha();
+
+        imprimearvoreOrder(arvore->raiz, opt);
+
+        desenharLinha();
+    }
     else
         {
             printf("\nValor inserido invalido\n");
         }
 }
+    system("pause");
 }
-
-int ProcuraNome(NoArvore* raiz, char *nome) {
-
-
-}
-
 
 float somaValores(NoArvore* raiz)
 {
@@ -315,5 +341,261 @@ float somaValores(NoArvore* raiz)
 
 
 
+NoArvore* inserirNovaVenda(Arvore* arvore)
+{
+    NoArvore *nova = (NoArvore*)malloc(sizeof(NoArvore));
+    Data data;
+    struct tm *hoje;
+    time_t segundos;
+    time(&segundos);
+    hoje = localtime(&segundos);
+    Dados dados;
 
+    printf("\n==================================\n");
+    printf("                    CADASTRO DE NOVA VENDA      \n");
+    printf("==================================\n");
+
+    int min = 1000;
+    int max = 9999;
+
+    dados.id = (rand() % (max - min + 1)) + min;
+
+    printf("ID gerado: %d\n", dados.id);
+    printf("Digite o nome do Cliente: ");
+    lerTexto(dados.cliente,50);
+
+    printf("Digite o nome do Vendedor: ");
+    lerTexto(dados.vendedor,50);
+   char* matriculaEncontrada = percorreArvore(arvore->raiz, dados.vendedor);
+
+   int confirmacao;
+    if (matriculaEncontrada != NULL)
+    {
+        printf("\nATENCAO: Vendedor '%s' ja encontrado com matricula [%s].\n", dados.vendedor, matriculaEncontrada);
+        do {
+            printf("Deseja usar este cadastro existente? \n");
+            printf("[1] Sim;\n");
+            printf("[2] Nao;\n");
+            printf("Opcao: ");
+            scanf("%d", &confirmacao);
+            empty_stdin();
+
+            if (confirmacao != 1 && confirmacao != 2) printf("Opcao invalida.\n");
+        } while (confirmacao != 1 && confirmacao != 2);
+
+        if (confirmacao == 1) {
+            strcpy(dados.matricula, matriculaEncontrada);
+            printf(">> Matricula mantida: %s\n", dados.matricula);
+        } else {
+            gerarMatricula(dados.matricula);
+            printf(">> Nova matricula gerada: %s\n", dados.matricula);
+        }
+    }
+    else
+    {
+        gerarMatricula(dados.matricula);
+        printf(">> Novo vendedor. Matricula gerada: %s\n", dados.matricula);
+    }
+    do {
+        printf("Digite a data de nascimento (dd mm aaaa):\n");
+        scanf("%d %d %d", &dados.data.dia, &dados.data.mes, &dados.data.ano);
+        empty_stdin();
+        if(dados.data.dia>31 || dados.data.dia <1 || dados.data.mes <1 || dados.data.mes>12 || dados.data.ano<1990 || dados.data.ano >hoje->tm_year+1900 || ((dados.data.dia>hoje->tm_mday && dados.data.mes>=hoje->tm_mon+1) || dados.data.ano>hoje->tm_year+1900)){
+            printf("Data invalida\n");
+        }
+  }while(dados.data.dia>31 || dados.data.dia <1 || dados.data.mes <1 || dados.data.mes>12 || dados.data.ano<1990 || dados.data.ano >hoje->tm_year+1900 || ((dados.data.dia>hoje->tm_mday && dados.data.mes>=hoje->tm_mon+1) || dados.data.ano>hoje->tm_year+1900));
+    do{
+        printf("Digite o valor da venda: ");
+        if(scanf("%f", &dados.valor) == 0) {
+             printf("Valor invalido (digite apenas numeros)\n");
+             empty_stdin();
+             dados.valor = -1;
+        } else {
+             empty_stdin();
+        }
+    } while(dados.valor <= 0);
+    printf("\nVenda confirmada!\n");
+    arvore->raiz = InsereArvore(arvore->raiz, dados.id, dados.cliente, dados.vendedor, dados.matricula, dados.data, dados.valor);
+    return arvore->raiz;
+}
+void buscarVendasVendedor(NoArvore *no, char *busca, int porMatricula)
+{
+    if(no!=NULL)
+    {
+            if ((porMatricula && strcasecmp(no->info.matricula, busca) == 0) || (!porMatricula && strcasecmp(no->info.vendedor, busca) == 0))
+                {
+                    imprimeNo(no);
+                }
+    buscarVendasVendedor(no->right, busca, porMatricula);
+    buscarVendasVendedor(no->left, busca, porMatricula);
+    }
+    else
+    {
+        printf("Nao ha venda registrada");
+    }
+}
+void buscarFuncaoTres(NoArvore *no, char *palavra, int opcao){
+     if(no != NULL){
+        if(opcao == 1){
+            if (strcasecmp(no->info.matricula, palavra) == 0){
+                imprimeNoBusca(no);
+            }
+            buscarFuncaoTres(no->right, palavra,opcao);
+            buscarFuncaoTres(no->left, palavra,opcao);
+        }
+        else{
+            if (strcasecmp(no->info.vendedor, palavra) == 0)
+            {
+                imprimeNoBusca(no);
+            }
+            buscarFuncaoTres(no->right, palavra,opcao);
+            buscarFuncaoTres(no->left, palavra,opcao);
+        }
+    }
+ }
+void buscarPorNome(NoArvore *no, char *nome)
+{
+    if(no != NULL)
+{
+    if (strcasecmp(no->info.vendedor, nome) == 0)
+    {
+        imprimeNo(no);
+    }
+    buscarPorNome(no->right, nome);
+    buscarPorNome(no->left, nome);
+}
+}
+
+void menuBuscarVendedor(Arvore *a){
+    int opcao;
+    char busca[51];
+    if ((VaziaArvore(a))==0){
+        printf("\n>> Nenhuma venda cadastrada para buscar.\n");
+        system("pause");
+    }
+else{
+    do{
+        printf("\n=== BUSCAR VENDAS ===\n");
+        printf("1. Por Matricula\n");
+        printf("2. Por Nome\n");
+        printf("Escolha: ");
+        scanf("%d", &opcao);
+        empty_stdin();
+        if(opcao < 1 || opcao > 2){
+                printf("\nOpcao de busca invalida.\n");
+                system("pause");
+
+        }
+    }while(opcao > 2 || opcao <= 0);
+    if (opcao == 1)
+    {
+        printf("Digite a matricula (ex: V000): ");
+        lerTexto(busca,50);
+    }
+    else
+    {
+        printf("Digite o nome do vendedor: ");
+        lerTexto(busca,50);
+    }
+        desenharDivisor();
+        printf("| %-6s | %-15s | %-10s | %-11s |\n",
+               "ID", "Cliente", "Data", "Valor");
+        desenharDivisor();
+        buscarFuncaoTres(a->raiz, busca,opcao);
+        system("pause");
+
+
+
+
+}
+}
+
+void Venda_maior_menor(Arvore *arvore)
+{
+    if(arvore->raiz == NULL)
+    {
+        printf("\nNao ha vendas registradas no momento\n");
+        system("pause");
+    }
+    else
+    {
+        float valor;
+        int opcao;
+        printf("\n=== FILTRAR POR VALOR ===\n");
+        printf("[1] Listar vendas MAIORES que X\n");
+        printf("[2] Listar vendas MENORES que X\n");
+        printf("Escolha a opcao: ");
+        scanf("%d", &opcao);
+        empty_stdin();
+
+        do{
+            printf("Digite o valor de referencia (R$): ");
+        if(scanf("%f", &valor) == 0) {
+             printf("Valor invalido (digite apenas numeros)\n");
+             empty_stdin();
+             valor = -1;
+        } else {
+             empty_stdin();
+        }
+    } while(valor <= 0);
+        desenharLinha();
+        printf("| %-6s | %-15s | %-9s | %-15s | %-10s | %-11s |\n",
+           "ID", "Vendedor", "Matricula", "Cliente", "Data", "Valor");
+        desenharLinha();
+        imprimeAcimaouAbaixo(arvore->raiz,valor,opcao);
+        system("pause");
+    }
+}
+void estatisticas(Arvore *arvore)
+{
+    if (VaziaArvore(arvore)== 1){
+    int totalVendas = quantidadeNos(arvore->raiz, 0);
+    float faturamentoTotal = somaValores(arvore->raiz);
+    float Media = faturamentoTotal / totalVendas;
+    if(totalVendas == 0){
+        Media = 0.0;
+    }
+        printf("\n");
+        printf("+---------------------------+-------------------+\n");
+        printf("|    RESUMO DE VENDAS       |      VALORES      |\n");
+        printf("+---------------------------+-------------------+\n");
+        printf("| Total de Vendas           | %-17d |\n", totalVendas);
+        printf("+---------------------------+-------------------+\n");
+        printf("| Faturamento Total (Bruto) | R$ %-14.2f |\n", faturamentoTotal);
+        printf("+---------------------------+-------------------+\n");
+        printf("| Ticket Medio por Venda    | R$ %-14.2f |\n", Media);
+        printf("+---------------------------+-------------------+\n");
+
+}
+    else
+    {
+        printf("\n[!] Nao ha vendas registradas para gerar estatisticas.\n");    }
+        system("pause");
+}
+Arvore* removerVenda(Arvore*arvore){
+    int id;
+    if ((VaziaArvore(arvore))==1){
+    printf("\n Remover Venda\n");
+    printf("Digite o ID da venda a ser removida: ");
+    scanf("%d", &id);
+    empty_stdin();
+    if (BuscarArvore(arvore->raiz, id)){
+        printf("\nVenda removida!\n");
+        arvore->raiz = RemoveGalho(arvore->raiz, id);
+        system("pause");
+        return arvore;
+    }
+    else{
+        printf("Venda com ID %d nao encontrada!\n", id);
+        system("pause");
+        return arvore;
+    }
+    }
+
+else
+{
+        printf("\nNenhuma venda cadastrada para remover.\n");
+        return arvore;
+}
+}
 #endif // ARVORE_H_INCLUDED
